0279d39627cba7ec77ba4d10bd718cd0fd22f9b4
techJagadeesh <renangijagadeesh2018@gmail.com>
2025-05-03 23:21:25 +0530

    Card Sample added

---
+++ src/CardDescription.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CardDescription:React.FC = ()=>{
+    return(
+       <p>An action-packed thrill ride, the movie follows a fearless
+         hero who must confront overwhelming odds to save the day. Intense 
+         fight sequences, high-speed chases, and explosive stunts keep the 
+         adrenaline rushing from start to finish.</p>
+    )
+}
+
+export default CardDescription;
---
+++ src/CardImage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CardTitle from "./CardTitle";
+
+const CardImage: React.FC = () => {
+  return (
+  <div>
+    <img src="/src/assets/Movie.svg" alt="Movie" />
+    <CardTitle/>
+  </div>
+)
+};
+
+export default CardImage;
